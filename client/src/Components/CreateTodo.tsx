13d86b332eab54739599323b185e5c4b3312a6b4
gift56 <efeasiughu@gmail.com>
2023-02-14 11:38:23 -0800

    working on client

--- client/src/Components/CreateTodo.tsx
+++ client/src/Components/CreateTodo.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import {useForm} from '@mantine/hooks'
 
 const CreateTodo = () => {
   const [open, setOpen] = useState(false);
+
+  const form = useForm();
+
   return <div>CreateTodo</div>;
 };
 
