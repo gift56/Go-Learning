6cddf1ad63c8c2bbd55d33b8ee845818303ba3a3
gift56 <efeasiughu@gmail.com>
2023-02-14 11:25:23 -0800

    working on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { Box } from "@mantine/core";
 import useSwR from "swr";
 
-const fetcher = (url: string) =>fetch(url).then((res)=>res.json())
+export const ENDPOINT=""
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const App = () => {
   return <Box>Hello world</Box>;
