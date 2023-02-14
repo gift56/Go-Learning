ae8664547cf31e2edd89892a9e53c3dd2f213133
gift56 <efeasiughu@gmail.com>
2023-02-14 11:27:38 -0800

    working on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,11 @@ import useSwR from "swr";
 
 export const ENDPOINT = "http://localhost:4000";
 
-const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((res) => res.json());
+const fetcher = (url: string) =>
+  fetch(`${ENDPOINT}/${url}`).then((res) => res.json());
 
 const App = () => {
+  const {} = useSwR();
   return <Box>Hello world</Box>;
 };
 
