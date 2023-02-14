ae1711e1b920264bfd15fbda943501779dc234d9
gift56 <efeasiughu@gmail.com>
2023-02-14 11:46:02 -0800

    working on client

--- client/src/Components/CreateTodo.tsx
+++ client/src/Components/CreateTodo.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
-
+import { useForm } from "@mantine/form";
 
 const CreateTodo = () => {
   const [open, setOpen] = useState(false);
 
- 
+  const form = useForm({
+    initialValues: {
+      title: "",
+      body: "",
+    },
+  });
 
   return <div>CreateTodo</div>;
 };
